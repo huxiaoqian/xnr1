64ae3dcfb3f4be19d7a913620bc30a1fd1a21cef
linhaobuaa <1257819385@qq.com>
2017-07-09 20:31:25 +0800

    cyz

---
+++ xnr/qq_xnr_operate/utils.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+use to save function---about deal database
+'''
+import sys
+from xnr.global_utils import es_xnr,qq_xnr_index_name,qq_xnr_index_type
+from xnr.parameter import MAX_VALUE
+
