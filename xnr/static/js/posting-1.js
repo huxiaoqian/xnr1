fca08f2e994fd277e6ec5f8db00344475ffce5aa
linhaobuaa <1257819385@qq.com>
2017-08-11 15:36:05 +0800

    ymm

--- xnr/static/js/posting-1.js
+++ xnr/static/js/posting-1.js
@@ -1,5 +1,8 @@
+var xnrUser='WXNR0001',
+//weiboMail='13121835733',weiboPhone='13121835733','&weibo_mail_account='+weiboMail+'&weibo_phone_account='+weiboPhone+
+    uid='1234567890',mid='4085480909715263';
 //@用户推荐
-var recommendUrl='/weibo_xnr_operate/daily_recommend_at_user/?xnr_user_no=WXNR0001';
+var recommendUrl='/weibo_xnr_operate/daily_recommend_at_user/?xnr_user_no='+xnrUser;
 public_ajax.call_request('get',recommendUrl,recommendlist);
 function recommendlist(data) {
     var str='';
@@ -11,8 +14,6 @@ function recommendlist(data) {
     $('#user_recommend .user_example_list').html(str);
 }
 //------
-var xnrUser='WXNR0001',weiboMail='13121835733',weiboPhone='13121835733',
-    uid='1234567890',mid='4085480909715263';
 var operateType,actType;
 function obtain(t) {
     if (t == 'o'){
@@ -32,7 +33,7 @@ $('#sure_post').on('click',function () {
 
     //原创
     var post_url_1='/weibo_xnr_operate/submit_tweet/?tweet_type='+actType+'&operate_type='+operateType+
-        '&xnr_user_no='+xnrUser+'&text='+txt+'&weibo_mail_account='+weiboMail+'&weibo_phone_account='+weiboPhone+'&rank='+rank+
+        '&xnr_user_no='+xnrUser+'&text='+txt+'&rank='+rank+
         '&p_url=["/home/ubuntu8/yuanhuiru/xnr/xnr1/xnr/images/1.jpg","/home/ubuntu8/yuanhuiru/xnr/xnr1/xnr/images/2.jpg"]';
     if ($("input[name='demo']")[0].checked){
         if ($('.start').val() && $('.end').val()){
@@ -52,21 +53,96 @@ function postYES(data) {
     console.log(data)
 }
 //语料推荐
+var defalutWeiboUrl='/weibo_xnr_operate/daily_recommend_tweets/?theme=旅游&sort_item=timestamp';
+public_ajax.call_request('get',defalutWeiboUrl,defalutWords)
 $('.everyday-2 .ed-2-1 input:radio[name="theme"]').on('click',function () {
-    var d=$('.everyday-2 .ed-2-2 .demo-radio');
-    for(var e=0;e<d.length;e++){if(d[e].checked) {d[e].checked=false;}};
+    //var d=$('.everyday-2 .ed-2-2 .demo-radio');
+    // for(var e=0;e<d.length;e++){if(d[e].checked) {d[e].checked=false;}};
     var the=$(this).val();
-    var the_url='/weibo_xnr_operate/daily_recommend_tweets/?theme='+the;
+    var theSort=$('.everyday-2 .ed-2-2 input:radio[name="th"]:checked').val();
+    var the_url='/weibo_xnr_operate/daily_recommend_tweets/?theme='+the+'&sort_item='+theSort;
     // public_ajax.call_request('get',the_url,postYES)
-})
+});
 $('.everyday-2 .ed-2-2 .demo-radio').on('click',function () {
     var TH=$(this).val();
     var the=$('.everyday-2 .ed-2-1 input:radio[name="theme"]:checked').val();
     var TH_url='/weibo_xnr_operate/daily_recommend_tweets/?theme='+the+'&sort_item='+TH;
+    console.log(TH_url)
     // public_ajax.call_request('get',TH_url,postYES)
-})
-
-
+});
+function defalutWords(data) {
+    console.log(data)
+    $('#defaultWeibo').bootstrapTable('load', data);
+    $('#defaultWeibo').bootstrapTable({
+        data:data,
+        search: true,//是否搜索
+        pagination: true,//是否分页
+        pageSize: 3,//单页记录数
+        pageList: [15,20,25],//分页步进值
+        sidePagination: "client",//服务端分页
+        searchAlign: "left",
+        searchOnEnterKey: false,//回车搜索
+        showRefresh: false,//刷新按钮
+        showColumns: false,//列选择按钮
+        buttonsAlign: "right",//按钮对齐方式
+        locale: "zh-CN",//中文支持
+        detailView: false,
+        showToggle:false,
+        sortName:'bci',
+        sortOrder:"desc",
+        columns: [
+            {
+                title: "",//标题
+                field: "",//键名
+                sortable: true,//是否可排序
+                order: "desc",//默认排序方式
+                align: "center",//水平
+                valign: "middle",//垂直
+                formatter: function (value, row, index) {
+                    var name,txt;
+                    if (row._source.nickname==''||row._source.speaker_nickname=='null'||row._source.speaker_nickname=='unknown'){
+                        name=row._source.speaker_qq_number;
+                    }else {
+                        name=row._source.speaker_nickname;
+                    };
+                    if (row._source.text==''||row._source.text=='null'||row._source.text=='unknown'){
+                        txt='暂无内容';
+                    }else {
+                        txt=row._source.text;
+                    };
+                    var str=
+                        '<div class="post_perfect">'+
+                        '   <div class="post_center-hot">'+
+                        '       <img src="/static/images/post-6.png" class="center_icon">'+
+                        '       <div class="center_rel">'+
+                        '           <a class="center_1" href="###" style="color: #f98077;">'+name+'</a>：'+
+                        '           <i class="mid" style="display: none;">'+row._source.mid+'</i>'+
+                        '           <i class="uid" style="display: none;">'+row._source.uid+'</i>'+
+                        '           <span class="center_2">'+txt+
+                        '           </span>'+
+                        '        </div>'+
+                        '        <div class="center_3">'+
+                        '           <span class="cen3-1"><i class="icon icon-share"></i>&nbsp;&nbsp;转发（'+row._source+'）</span>'+
+                        '           <span class="cen3-2"><i class="icon icon-comments-alt"></i>&nbsp;&nbsp;评论（'+row._source+'）</span>'+
+                        '           <span class="cen3-3"><i class="icon icon-thumbs-up"></i>&nbsp;&nbsp;赞（'+row._source+'）</span>'+
+                        '        </div>'+
+                        '        <div class="commentDown" style="width: 100%;display: none;">'+
+                        '           <input type="text" class="comtnt" placeholder="评论内容"/>'+
+                        '           <span class="sureCom">评论</span>'+
+                        '        </div>'+
+                        '    </div>'+
+                        '</div>';
+                    return str;
+                    //评论
+                    $('.post_perfect .cen3-2').on('click',function () {
+                        $(this).parents('.post_perfect').find('.commentDown').show();
+                    });
+                }
+            },
+        ],
+    });
+    $('.defaultWeibo .search .form-control').attr('placeholder','输入关键词快速搜索相关微博（回车搜索）');
+}
 
 
 
