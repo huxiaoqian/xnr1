8503119e33847379817702065a9c975a39da7e88
linhaobuaa <1257819385@qq.com>
2017-08-21 19:21:51 +0800

    cyz

--- xnr/qq/sendQQGroupMessage.py
+++ xnr/qq/sendQQGroupMessage.py
@@ -9,7 +9,22 @@ import types
 from qqbot import _bot as bot
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch("http://219.224.134.213:9205/")
+import sys
+reload(sys)
+sys.path.append('../')
+from global_utils import es_xnr as es
+from global_utils import group_message_index_name_pre, group_message_index_type, sent_group_message_index_name_pre
+from qq_xnr_groupmessage_mappings import group_message_mappings
+# es = Elasticsearch("http://219.224.134.213:9205/")
+
+def sendfromweb(group,content):
+    
+    try:
+        result = sendMessage(bot,group,content)
+    except:
+        bot.Login()
+        result = sendMessage(bot,group,content)
+    return result
 
 def sendMessage(bot, group, content):
     bot.Update('group')
@@ -32,13 +47,19 @@ def sendMessage(bot, group, content):
         conMD5 = string_md5(content)
 
         nowDate = datetime.datetime.now().strftime('%Y-%m-%d')
-        index_name = 'group_message_' + str(nowDate)
+        index_name = sent_group_message_index_name_pre + str(nowDate)
         index_id = bot.conf.qq + '_' + gl[0].qq + '_' + str(int(round(time.time()))) + '_' + conMD5
-        es.index(index=index_name, doc_type='record', id=index_id, body=qq_item)
+        # 将发送的信息存入es
+        if not es.indices.exists(index=index_name):
+            group_message_mappings(bot.session.qq,nowDate)
+
+        es.index(index=index_name, doc_type=group_message_index_type, id=index_id, body=qq_item)
 
         bot.SendTo(gl[0], content)
+        return 1
     else:
         print 'you no own this group', group
+        return 0
 
 
 def string_md5(str):
@@ -51,5 +72,6 @@ def string_md5(str):
 
 
 if __name__ == '__main__':
-    bot.Login()
-    sendMessage(bot, 'SPDJ5', 'hi everyone')
+    # bot.Login()
+    # sendMessage(bot, 'SPDJ5', 'hi everyone')
+    sendfromweb('16美赛二群', 'o')
