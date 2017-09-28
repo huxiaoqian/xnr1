643ae852585a85accc4ea10312c321a3d192046b
linhaobuaa <1257819385@qq.com>
2017-09-28 16:49:06 +0800

    hxq

--- xnr/qq_xnr_report_manage/utils.py
+++ xnr/qq_xnr_report_manage/utils.py
@@ -5,7 +5,31 @@ qq_xnr report manage function
 import os
 import json
 import time
-#from xnr.global_utils import 
+from xnr.global_utils import es_xnr,qq_report_management_index_name,\
+        qq_report_management_index_type
 from xnr.time_utils import ts2yeartime,ts2datetime,datetime2ts
-from xnr.parameter import USER_NUM
+from xnr.parameter import USER_NUM, MAX_SEARCH_SIZE
 from xnr.global_config import S_TYPE
+
+
+def show_report_content(report_type, start_ts, end_ts, qq_xnr_no):
+    result = []
+    query_body = {
+            'query':{
+                        'bool':{
+                            'must':[
+                                {'term':{'report_type': report_type}},
+                                {'range':{'report_time':{'gt':start_ts, 'lt':end_ts}}},
+                                {'term':{'xnr_user_no': qq_xnr_no}}
+                                ]
+                            }
+                },
+            'size': MAX_SEARCH_SIZE,
+            'sort': [{'report_time':{'order':'desc'}}]
+            }
+    es_result = es_xnr.search(index=qq_report_management_index_name, \
+            doc_type=qq_report_management_index_type, body=query_body)['hits']['hits']
+    print 'es_result:', es_result
+    if es_result:
+        result = [item['_source'] for item in es_result]
+    return result
