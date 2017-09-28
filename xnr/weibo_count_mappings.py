8c4161846b0a5a63d1c8e79311470b2f2ec5d72c
linhaobuaa <1257819385@qq.com>
2017-09-28 10:18:11 +0800

    hxq

--- xnr/weibo_count_mappings.py
+++ xnr/weibo_count_mappings.py
@@ -4,7 +4,8 @@ import sys
 import json
 from elasticsearch import Elasticsearch
 from global_utils import es_xnr as es
-from global_utils import weibo_xnr_count_info_index_name,weibo_xnr_count_info_index_type
+from global_utils import weibo_xnr_count_info_index_name,weibo_xnr_count_info_index_type,\
+                         weibo_keyword_count_index_name,weibo_keyword_count_index_type
 
 
 def weibo_xnr_count_info_mappings():
@@ -60,5 +61,39 @@ def weibo_xnr_count_info_mappings():
         es.indices.create(index=weibo_xnr_count_info_index_name,body=index_info,ignore=400)
 
 
+def weibo_keyword_count_mappings():
+    index_info = {
+        'settings':{
+            'number_of_replicas':0,
+            'number_of_shards':5
+        },
+        'mappings':{
+            weibo_keyword_count_index_type:{
+                'properties':{            
+                    'xnr_user_no':{               # 虚拟人编号
+                        'type':'string',
+                        'index':'not_analyzed'
+                    },
+                    'date_time':{                #日期，例如：2017-09-07
+                        'type':'string',
+                        'index':'not_analyzed'
+                    },
+                    'keyword_value_string':{                #关键词统计结果
+                        'type':'string',
+                        'index':'no'
+                    },
+                    'timestamp':{ # 时间戳
+                        'type':'long'
+                    }
+                }
+            }
+        }
+    }
+    exist_indice=es.indices.exists(index=weibo_keyword_count_index_name)
+    if not exist_indice:
+        es.indices.create(index=weibo_keyword_count_index_name,body=index_info,ignore=400)
+
+
 if __name__=='__main__':
-	weibo_xnr_count_info_mappings()
+	#weibo_xnr_count_info_mappings()
+    weibo_keyword_count_mappings()
