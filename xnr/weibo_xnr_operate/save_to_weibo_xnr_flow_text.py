c87aed03e509abb1a5616b9e71dcd141cc8d9dc9
linhaobuaa <1257819385@qq.com>
2017-10-08 15:30:56 +0800

    yuanhuiru

--- xnr/weibo_xnr_operate/save_to_weibo_xnr_flow_text.py
+++ xnr/weibo_xnr_operate/save_to_weibo_xnr_flow_text.py
@@ -14,10 +14,16 @@ def save_to_xnr_flow_text(tweet_type,xnr_user_no,text):
 	xnr_flow_text_index_name = xnr_flow_text_index_name_pre + current_date
 
 	item_detail = {}
+	item_detail['uid'] = xnr_user_no2uid(xnr_user_no)
 	item_detail['xnr_user_no'] = xnr_user_no
 	item_detail['text'] = text
 	item_detail['task_source'] = tweet_type
+	#item_detail['topic_field'] = ''
+	item_detail['mid'] = ''
 	task_id = xnr_user_no + '_' + str(current_time)
+    
+    #classify_results = topic_classfiy(classify_mid_list, classify_text_dict)
+
 	try:
 		weibo_xnr_flow_text_mappings(xnr_flow_text_index_name)
 		es.index(index=xnr_flow_text_index_name,doc_type=xnr_flow_text_index_type,\
