1ea81d7c69404a1807176bde923298a9a9311d82
linhaobuaa <1257819385@qq.com>
2017-10-12 10:30:16 +0800

    qxk

--- xnr/system_manage/utils.py
+++ xnr/system_manage/utils.py
@@ -4,11 +4,14 @@ use to manage the system
 '''
 import os
 import json
+import sqlite
+import sqlite3
 from xnr.global_utils import es_xnr as es
-from xnr.global_utils import weibo_log_management_index_name,weibo_log_management_index_type,\
+from xnr.global_utils import weibo_xnr_index_name,weibo_xnr_index_type,\
+                        weibo_log_management_index_name,weibo_log_management_index_type,\
 						weibo_authority_management_index_name,weibo_authority_management_index_type,\
 						weibo_account_management_index_name,weibo_account_management_index_type
-from xnr.parameter import MAX_VALUE
+from xnr.parameter import MAX_VALUE,USER_XNR_NUM
 
 ##############################################################
 ########	log management
@@ -46,7 +49,9 @@ def show_log_list():
 	results=[]
 	for item in result:
 		item['_source']['log_id']=item['_id']
+		#item['_source']['operate_content']=json.loads(item['_source']['operate_content'])
 		results.append(item['_source'])
+		#print results
 	return results
 
 #delete log list
@@ -149,6 +154,61 @@ def add_user_xnraccount(account_id,xnr_accountid):
 
 
 #show all users account
+#连接数据库,获取账户列表
+def get_user_account_list():     
+    cx = sqlite3.connect("/home/ubuntu8/yuanhuiru/xnr/xnr1/xnr/flask-admin.db")
+    cu=cx.cursor()
+    cu.execute("select email from user") 
+    user_info = cu.fetchall()
+    cx.close()
+    return user_info
+
+#根据账户名称查询所管理的虚拟人
+def get_user_xnr_list(user_account,status_start,status_end):
+    query_body={
+        'query':{
+        	'filtered':{
+        		'filter':{
+        			'bool':{
+        			    'must':[
+        			    	{'term':{'submitter':user_account}},
+        			    	{'range':{'create_status':{'gte':status_start,'lt':status_end}}}
+        			    ]
+        			}
+        		}
+        	}
+        },
+        'size':USER_XNR_NUM
+    }
+    try:
+        user_result=es.search(index=weibo_xnr_index_name,doc_type=weibo_xnr_index_type,body=query_body)['hits']['hits']
+        xnr_user_no_list=[]
+        for item in user_result:
+            xnr_user_no_list.append(item['_source']['xnr_user_no'])
+    except:
+        xnr_user_no_list=[]
+    return xnr_user_no_list
+
+
+#整合账户管理
+def show_users_account():
+    account_list=get_user_account_list()
+    print account_list
+    account_info=[]
+    for account_name in account_list:
+        account_dict=dict()
+        account_dict['user_name']=account_name
+        #已完成虚拟人
+        status_zero=0
+        status_second=2
+        status_three=3
+        account_dict['complete_xnr']=get_user_xnr_list(account_name,status_zero,status_second)
+        #未完成虚拟人
+        account_dict['uncomplete_xnr']=get_user_xnr_list(account_name,status_second,status_three)
+        account_info.append(account_dict)
+    return account_info
+
+'''
 def show_users_account():
 	query_body={
 		'query':{
@@ -161,7 +221,7 @@ def show_users_account():
 	for item in result:
 		results.append(item['_source'])
 	return results
-
+'''
 #delete account
 #step 1:delete the user account
 def delete_user_account(account_id):
