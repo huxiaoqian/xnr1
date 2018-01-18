1284e972ce51cee56f84a5e4951264ee9260dd12
linhaobuaa <1257819385@qq.com>
2018-01-18 18:31:13 +0800

    aaa

--- xnr/index/utils.py
+++ xnr/index/utils.py
@@ -7,7 +7,12 @@ sys.path.append(trans_path)
 from trans import trans as text_trans
 
 def utils_text_trans(q):
-    return text_trans(q)
+    q_list = [q]
+    result_list = text_trans(q_list)
+    try:
+        return result_list[0]
+    except:
+        return ''
 
 def utils_voice_trans(res):
     pass
