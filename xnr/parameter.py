2e2203375f0127cd73124773402ebe8ad9919096
linhaobuaa <1257819385@qq.com>
2018-01-24 14:26:17 +0800

    aa

--- xnr/parameter.py
+++ xnr/parameter.py
@@ -180,11 +180,11 @@ task_source_ch2en = {u'日常发帖':'daily_post',u'热门发帖':'hot_post',u'�
 #facebook
 fb_domain_ch2en_dict = {u'高校':'university',u'机构':'admin',u'媒体':'media',u'民间组织':'folkorg',\
                         u'法律机构及人士':'lawyer',u'政府机构及人士':'politician',u'媒体人士':'mediaworker',\
-                        u'活跃人士':'activer',u'其他':'other',u'商业人士':'business'}
+                        u'活跃人士':'active',u'其他':'other',u'商业人士':'business'}
                         
 fb_domain_en2ch_dict = {'university':u'高校','admin':u'机构','media':u'媒体',\
                         'folkorg':u'民间组织','lawyer':u'法律机构及人士','politician':u'政府机构及人士',\
-                        'mediaworker':u'媒体人士','activer':u'活跃人士','other':u'其他','business':u'商业人士'}               
+                        'mediaworker':u'媒体人士','active':u'活跃人士','other':u'其他','business':u'商业人士'}               
 
 #facebook&twitter
 fb_tw_topic_en2ch_dict = {'life':u'其他类','law':u'民生类_法律','computer':u'科技类','house':u'民生类_住房',\
