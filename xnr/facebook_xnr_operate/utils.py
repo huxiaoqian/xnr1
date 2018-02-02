124d866ff0fddbf042c501a09e252d08b79c417e
linhaobuaa <1257819385@qq.com>
2018-02-03 01:34:02 +0800

    a

--- xnr/facebook_xnr_operate/utils.py
+++ xnr/facebook_xnr_operate/utils.py
@@ -1007,7 +1007,12 @@ from xnr.global_utils import facebook_feedback_comment_index_name, facebook_feed
                             facebook_feedback_private_index_name, facebook_feedback_private_index_type,\
                             facebook_feedback_at_index_name, facebook_feedback_at_index_type,\
                             facebook_feedback_fans_index_name, facebook_feedback_fans_index_type
-from xnr.time_utils import get_timeset_indexset_list
+from xnr.time_utils import get_timeset_indexset_list, fb_get_flow_text_index_list as get_flow_text_index_list
+from xnr.utils import judge_fb_follow_type, judge_fb_sensing_sensor
+from xnr.parameter import TOP_ACTIVE_SOCIAL
+trans_path = os.path.join(os.path.abspath(os.getcwd()), 'xnr/cron/trans/')
+sys.path.append(trans_path)
+from trans import trans, simplified2traditional
 
 def get_show_comment(task_detail):
     xnr_user_no = task_detail['xnr_user_no']
@@ -1187,4 +1192,310 @@ def get_show_fans(task_detail):
                 results_all.append(item['_source'])
     except Exception,e:
         print e
+    return results_all
+
+# 主动社交-直接搜索
+def get_direct_search(task_detail):
+    return_results_all = []
+    xnr_user_no = task_detail['xnr_user_no']
+    uid_list = task_detail['uid_list']
+    for uid in uid_list:
+        query_body = {
+            'query':{
+                'filtered':{
+                    'filter':{
+                        'term':{'uid':uid}
+                    }
+                }
+            }
+        }
+        es_results = es.search(index=fb_portrait_index_name,doc_type=fb_portrait_index_type,body=query_body)['hits']['hits']
+        if es_results:
+            for item in es_results:
+                uid = item['_source']['uid']
+                nick_name,photo_url = fb_uid2nick_name_photo(uid)
+                item['_source']['nick_name'] = nick_name
+                item['_source']['photo_url'] = photo_url
+                fb_type = judge_fb_follow_type(xnr_user_no,uid)
+                sensor_mark = judge_fb_sensing_sensor(xnr_user_no,uid)
+
+                item['_source']['fb_type'] = fb_type
+                item['_source']['sensor_mark'] = sensor_mark
+
+            
+                if S_TYPE == 'test':
+                    current_time = datetime2ts(S_DATE)
+                else:
+                    current_time = int(time.time())
+
+                index_name = get_flow_text_index_list(current_time)
+
+                query_body = {
+                    'query':{
+                        'bool':{
+                            'must':[
+                                {'term':{'uid':uid}},
+                                # {'terms':{'message_type':[1,3]}}
+                            ]
+                        }
+                    },
+                    # 'sort':{'retweeted':{'order':'desc'}}
+                }
+
+                es_fb_results = es.search(index=index_name,doc_type=facebook_flow_text_index_type,body=query_body)['hits']['hits']
+
+                fb_list = []
+                for fb in es_fb_results:
+                    fb = fb['_source']
+                    fb_list.append(fb)
+                item['_source']['fb_list'] = fb_list
+                item['_source']['portrait_status'] = True
+                return_results_all.append(item['_source'])
+        else:
+            item_else = dict()
+            item_else['uid'] = uid
+            nick_name,photo_url = fb_uid2nick_name_photo(uid)
+            item_else['nick_name'] = nick_name
+            item_else['photo_url'] = photo_url
+            fb_type = judge_fb_follow_type(xnr_user_no,uid)
+            sensor_mark = judge_fb_sensing_sensor(xnr_user_no,uid)
+            item_else['fb_type'] = fb_type
+            item_else['sensor_mark'] = sensor_mark
+            item_else['portrait_status'] = False
+          
+            if S_TYPE == 'test':
+                current_time = datetime2ts(S_DATE_FB)
+            else:
+                current_time = int(time.time())
+
+            index_name = get_flow_text_index_list(current_time)
+
+            query_body = {
+                'query':{
+                    'term':{'uid':uid}
+                },
+                # 'sort':{'retweeted':{'order':'desc'}}
+            }
+
+            es_fb_results = es.search(index=index_name,doc_type=facebook_flow_text_index_type,body=query_body)['hits']['hits']
+
+            fb_list = []
+            for fb in es_fb_results:
+                item_else['user_fansnum'] = fb['_source']['user_fansnum']
+                fb = fb['_source']
+                fb_list.append(fb)
+            item_else['fb_list'] = fb_list
+            return_results_all.append(item_else)
+    return return_results_all
+
+## 主动社交- 相关推荐
+def get_related_recommendation(task_detail):
+    avg_sort_uid_dict = {}
+    xnr_user_no = task_detail['xnr_user_no']
+    sort_item = task_detail['sort_item']
+    es_result = es.get(index=fb_xnr_index_name,doc_type=fb_xnr_index_type,id=xnr_user_no)['_source']
+    uid = es_result['uid']
+    monitor_keywords = es_result['monitor_keywords']
+    monitor_keywords_list = monitor_keywords.split(',')
+
+
+    ## 监测词关注
+    nest_query_list = []
+    #文本中可能存在英文或者繁体字，所以都匹配一下
+    monitor_en_keywords_list = trans(monitor_keywords_list, target_language='en')
+    for i in range(len(monitor_keywords_list)):
+        monitor_keyword = monitor_keywords_list[i]
+        monitor_traditional_keyword = simplified2traditional(monitor_keyword)
+        
+        if len(monitor_en_keywords_list) == len(monitor_keywords_list): #确保翻译没出错
+            monitor_en_keyword = monitor_en_keywords_list[i]
+            nest_query_list.append({'wildcard':{'keywords_string':'*'+monitor_en_keyword+'*'}})
+        
+        nest_query_list.append({'wildcard':{'keywords_string':'*'+monitor_keyword+'*'}})
+        nest_query_list.append({'wildcard':{'keywords_string':'*'+monitor_traditional_keyword+'*'}})
+
+
+    recommend_list_r = es.get(index=fb_xnr_fans_followers_index_name,doc_type=fb_xnr_fans_followers_index_type,id=xnr_user_no)['_source']
+    recommend_list = []
+    if recommend_list_r.has_key('followers_list'):
+        recommend_list = recommend_list_r['followers_list']
+    recommend_set_list = list(set(recommend_list))
+    if S_TYPE == 'test':
+        current_date = S_DATE_FB
+    else:
+        current_date = int(time.time()-24*3600)
+    flow_text_index_name = facebook_flow_text_index_name_pre + current_date
+    if sort_item != 'friend':
+        uid_list = []
+        if sort_item == 'influence':
+            sort_item = 'user_fansnum'
+        query_body_rec = {
+            'query':{
+                'bool':{
+                    'should':nest_query_list
+                }
+            },
+            'aggs':{
+                'uid_list':{
+                    'terms':{'field':'uid','size':TOP_ACTIVE_SOCIAL,'order':{'avg_sort':'desc'} },
+                    'aggs':{'avg_sort':{'avg':{'field':sort_item}}}
+                }
+            }
+        }
+
+        es_rec_result = es.search(index=flow_text_index_name,doc_type='text',body=query_body_rec)['aggregations']['uid_list']['buckets']
+        
+        for item in es_rec_result:
+            uid = item['key']
+            uid_list.append(uid)
+            
+            avg_sort_uid_dict[uid] = {}
+
+            if sort_item == 'user_fansnum':
+                avg_sort_uid_dict[uid]['sort_item_value'] = int(item['avg_sort']['value'])
+            else:
+                avg_sort_uid_dict[uid]['sort_item_value'] = round(item['avg_sort']['value'],2)
+
+    else:
+        if S_TYPE == 'test':
+            # uid_list = FRIEND_LIST
+            uid_list = [] 
+        else:
+            uid_list = []
+            friends_list_results = es.mget(index=facebook_user_index_name,doc_type=facebook_user_index_type,body={'ids':recommend_set_list})['_source']
+            for result in friends_list_results:
+                friends_list = friends_list + result['friend_list']
+            friends_set_list = list(set(friends_list))
+            sort_item_new = 'fansnum'
+            query_body_rec = {
+                'query':{
+                    'bool':{
+                        'must':[
+                            {'terms':{'uid':friends_set_list}},
+                            {'bool':{
+                                'should':nest_query_list
+                            }}
+                        ]
+                    }
+                },
+                'aggs':{
+                    'uid_list':{
+                        'terms':{'field':'uid','size':TOP_ACTIVE_SOCIAL,'order':{'avg_sort':'desc'} },
+                        'aggs':{'avg_sort':{'avg':{'field':sort_item_new}}}
+                    }
+                }
+            }
+            es_friend_result = es.search(index=flow_text_index_name,doc_type='text',body=query_body_rec)['aggregations']['uid_list']['buckets']
+            
+            for item in es_friend_result:
+                uid = item['key']
+                uid_list.append(uid)
+                
+                avg_sort_uid_dict[uid] = {}
+                avg_sort_uid_dict[uid]['sort_item_value'] = int(item['avg_sort']['value'])
+                
+    results_all = []
+
+    for uid in uid_list:
+        query_body = {
+            'query':{
+                'filtered':{
+                    'filter':{
+                        'term':{'uid':uid}
+                    }
+                }
+            }
+        }
+
+        es_results = es.search(index=facebook_user_index_name,doc_type=facebook_user_index_type,body=query_body)['hits']['hits']
+        if es_results:
+            for item in es_results:
+                uid = item['_source']['uid']
+                nick_name,photo_url = fb_uid2nick_name_photo(uid)
+                item['_source']['nick_name'] = nick_name
+                item['_source']['photo_url'] = photo_url
+                fb_type = judge_fb_follow_type(xnr_user_no,uid)
+                sensor_mark = judge_fb_sensing_sensor(xnr_user_no,uid)
+
+                item['_source']['fb_type'] = fb_type
+                item['_source']['sensor_mark'] = sensor_mark
+
+                if sort_item == 'friend':
+                    if S_TYPE == 'test':
+                        item['_source']['fansnum'] = item['_source']['fansnum']
+                    else:
+                        item['_source']['fansnum'] = avg_sort_uid_dict[uid]['sort_item_value']
+                elif sort_item == 'sensitive':
+                    item['_source']['sensitive'] = avg_sort_uid_dict[uid]['sort_item_value']
+                    item['_source']['fansnum'] = item['_source']['fansnum']
+                else:
+                    item['_source']['fansnum'] = avg_sort_uid_dict[uid]['sort_item_value']
+
+                if S_TYPE == 'test':
+                    current_time = datetime2ts(S_DATE)
+                else:
+                    current_time = int(time.time())
+
+                index_name = get_flow_text_index_list(current_time)
+                query_body = {
+                    'query':{
+                        'bool':{
+                            'must':[
+                                {'term':{'uid':uid}},
+                                {'terms':{'message_type':[1,3]}}
+                            ]
+                        }
+                    },
+                    # 'sort':{'retweeted':{'order':'desc'}}
+                }
+
+                es_fb_results = es.search(index=index_name,doc_type=facebook_flow_text_index_type,body=query_body)['hits']['hits']
+                fb_list = []
+                for fb in es_fb_results:
+                    fb = fb['_source']
+                    fb_list.append(fb)
+                item['_source']['fb_list'] = fb_list
+                item['_source']['portrait_status'] = True
+                results_all.append(item['_source'])
+        else:
+            item_else = dict()
+            item_else['uid'] = uid
+            nick_name,photo_url = fb_uid2nick_name_photo(uid)
+            item_else['nick_name'] = nick_name
+            item_else['photo_url'] = photo_url
+            fb_type = judge_fb_follow_type(xnr_user_no,uid)
+            sensor_mark = judge_fb_sensing_sensor(xnr_user_no,uid)
+            item_else['fb_type'] = fb_type
+            item_else['sensor_mark'] = sensor_mark
+            item_else['portrait_status'] = False         
+
+            if S_TYPE == 'test':
+                current_time = datetime2ts(S_DATE_FB)
+            else:
+                current_time = int(time.time())
+
+            index_name = get_flow_text_index_list(current_time)
+
+            query_body = {
+                'query':{
+                    'term':{'uid':uid}
+                },
+                # 'sort':{'retweeted':{'order':'desc'}}
+            }
+
+            es_fb_results = es.search(index=index_name,doc_type=facebook_flow_text_index_type,body=query_body)['hits']['hits']
+
+            fb_list = []
+            for weibo in es_fb_results:
+                item_else['fansnum'] = fb['_source']['user_fansnum']
+                fb = fb['_source']
+                fb_list.append(weibo)
+            item_else['fb_list'] = fb_list
+            item_else['friendsnum'] = 0
+            item_else['statusnum'] = 0
+            if sort_item == 'sensitive':
+                item_else['sensitive'] = avg_sort_uid_dict[uid]['sort_item_value']
+            else:
+                item_else['fansnum'] = avg_sort_uid_dict[uid]['sort_item_value']
+            results_all.append(item_else)
     return results_all
