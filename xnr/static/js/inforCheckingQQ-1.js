5dcd6b206c220320f7b9762ec2524f19edd208eb
linhaobuaa <1257819385@qq.com>
2018-03-11 11:34:32 +0800

    ymm

--- xnr/static/js/inforCheckingQQ-1.js
+++ xnr/static/js/inforCheckingQQ-1.js
@@ -1,9 +1,9 @@
 //敏感消息
 var senNews_url='/qq_xnr_monitor/search_by_xnr_number/?xnr_number='+userQQnum+'&date='+(Number(Date.parse(new Date()))/1000);
-console.log(senNews_url)
 public_ajax.call_request('get',senNews_url,senNews);
 function senNews(data) {
-    var news=data.hits.hits;
+    var news=data;
+    console.log(news)
     $('#content-1-word').bootstrapTable('load', news);
     $('#content-1-word').bootstrapTable({
         data:news,
@@ -32,16 +32,16 @@ function senNews(data) {
                 valign: "middle",//垂直
                 formatter: function (value, row, index) {
                     var name,txt;
-                    if (row._source.qq_group_nickname==''||row._source.qq_group_nickname=='null'||row._source.qq_group_nickname=='unknown'){
-                        name=row._source.qq_group_number;
+                    if (row.qq_group_nickname==''||row.qq_group_nickname=='null'||row.qq_group_nickname=='unknown'){
+                        name=row.qq_group_number;
                     }else {
-                        name=row._source.qq_group_nickname;
+                        name=row.qq_group_nickname;
                     };
-                    if (row._source.text==''||row._source.text=='null'||row._source.text=='unknown'){
+                    if (row.text==''||row.text=='null'||row.text=='unknown'){
                         txt='暂无内容';
                     }else {
-                        var keyword=row._source.sensitive_words_string.split('&');
-                        var s=row._source.text;
+                        var keyword=row.sensitive_words_string.split('&');
+                        var s=row.text;
                         for (var f=0;f<keyword.length;f++){
                             s=s.toString().replace(new RegExp(keyword[f],'g'),'<b style="color:#ef3e3e;">'+keyword[f]+'</b>');
                         }
@@ -53,8 +53,8 @@ function senNews(data) {
                         '       <div class="center_rel" style="text-align: left;">'+
                         '           <img src="/static/images/post-6.png" class="center_icon">'+
                         '           <a class="center_1" href="###" style="color:blanchedalmond;font-weight: 700;">'+
-                        '               <b class="name">'+name+'</b> <span>（</span><b class="QQnum">'+row._source.qq_group_number+'</b><span>）</span>' +
-                        '               <b class="time" style="display: inline-block;margin-left: 30px;""><i class="icon icon-time"></i>&nbsp;'+getLocalTime(row._source.timestamp)+'</b>  '+
+                        '               <b class="name">'+name+'</b> <span>（</span><b class="QQnum">'+row.qq_group_number+'</b><span>）</span>' +
+                        '               <b class="time" style="display: inline-block;margin-left: 30px;""><i class="icon icon-time"></i>&nbsp;'+getLocalTime(row.timestamp)+'</b>  '+
                         '               <span class="joinWord" onclick="joinWord(this)" tp="content">上报</span>'+
                         '           </a>'+
                         '           <div class="center_2" style="margin-top: 10px;"><b style="color:#ff5722;font-weight: 700;">摘要内容：</b><span>'+txt+'</span></div>'+
@@ -72,7 +72,6 @@ function senNews(data) {
 var senUserurl='/qq_xnr_monitor/show_sensitive_users/?xnr_number='+userQQnum;
 public_ajax.call_request('get',senUserurl,senUser);
 function senUser(data) {
-    console.log(data)
     $('#hot-2').bootstrapTable('load', data);
     $('#hot-2').bootstrapTable({
         data:data,
@@ -92,14 +91,14 @@ function senUser(data) {
         sortName:'bci',
         sortOrder:"desc",
         columns: [
-            {
-                title: "QQ号码",//标题
-                field: "qq_number",//键名
-                sortable: true,//是否可排序
-                order: "desc",//默认排序方式
-                align: "center",//水平
-                valign: "middle",//垂直
-            },
+            // {
+            //     title: "QQ号码",//标题
+            //     field: "qq_number",//键名
+            //     sortable: true,//是否可排序
+            //     order: "desc",//默认排序方式
+            //     align: "center",//水平
+            //     valign: "middle",//垂直
+            // },
             {
                 title: "昵称",//标题
                 field: "qq_nick",//键名
@@ -222,7 +221,7 @@ function joinWord(_this) {
     uploadData=JSON.stringify(uploadData);
     $.ajax({
         type:'get',
-        url:'/qq_xnr_monitor/report_warming_content/',
+        url:'/qq_xnr_monitor/report_warming_content_new/',
         async:true,
         dataType:"json",
         data:uploadData,
