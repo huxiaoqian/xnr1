ad2cb7fdadaee3e66308c545fb3af4b7298e3e5c
linhaobuaa <1257819385@qq.com>
2018-03-22 20:51:59 +0800

    a

--- xnr/facebook_count_mappings.py
+++ xnr/facebook_count_mappings.py
@@ -17,31 +17,93 @@ def facebook_xnr_count_info_mappings():
         'mappings':{
             facebook_xnr_count_info_index_type:{
               'properties': {
-                'fans_total_num': {
+                'comment_day_num': {
                   'type': 'long'
                 },
-                'fans_day_num': {
+                'at_total_num': {
                   'type': 'long'
                 },
-                'fans_growth_rate': {
+                'user_fansnum': {
+                  'type': 'long'
+                },
+                'xnr_user_no': {
+                  'index': 'not_analyzed',
+                  'type': 'string'
+                },
+                'comment_total_num': {
+                  'type': 'long'
+                },
+                'timestamp': {
+                  'type': 'long'
+                },
+                'private_total_num': {
+                  'type': 'long'
+                },
+                'safe': {
+                  'type': 'long'
+                },
+                'influence': {
                   'type': 'long'
                 },
                 'retweet_total_num': {
                   'type': 'long'
                 },
-                'retweet_day_num': {
+                'fans_group_sensitive_info': {
                   'type': 'long'
+                },
+                'feedback_total_sensitive_info': {
+                  'type': 'long'
+                },
+                'like_growth_rate': {
+                  'type': 'long'
+                },
+                'penetration': {
+                  'type': 'long'
+                },
+                'at_day_num': {
+                  'type': 'long'
+                },
+                'private_growth_rate': {
+                  'type': 'long'
+                },
+                'private_day_num': {
+                  'type': 'long'
+                },
+                'fans_growth_rate': {
+                  'type': 'long'
+                },
+                'fans_total_num': {
+                  'type': 'long'
+                },
+                'fans_num': {
+                  'type': 'long'
+                },
+                'fans_day_num': {
+                  'type': 'long'
+                },
+                'date_time': {
+                  'index': 'not_analyzed',
+                  'type': 'string'
                 },
                 'retweet_growth_rate': {
                   'type': 'long'
                 },
-                'comment_total_num': {
+                'daily_post_num': {
                   'type': 'long'
                 },
-                'comment_day_num': {
+                'business_post_num': {
+                  'type': 'long'
+                },
+                'at_growth_rate': {
                   'type': 'long'
                 },
                 'comment_growth_rate': {
+                  'type': 'long'
+                },
+                'trace_follow_tweet': {
+                  'type': 'long'
+                },
+                'follow_group_sensitive_info': {
                   'type': 'long'
                 },
                 'like_total_num': {
@@ -50,40 +112,22 @@ def facebook_xnr_count_info_mappings():
                 'like_day_num': {
                   'type': 'long'
                 },
-                'like_growth_rate': {
-                  'type': 'long'
-                },
-                'at_total_num': {
-                  'type': 'long'
-                },
-                'at_day_num': {
-                  'type': 'long'
-                },
-                'at_growth_rate': {
-                  'type': 'long'
-                },
-                'private_total_num': {
-                  'type': 'long'
-                },
-                'private_day_num': {
-                  'type': 'long'
-                },
-                'private_growth_rate': {
-                  'type': 'long'
-                },
-                'follow_group_sensitive_info': {
-                  'type': 'long'
-                },
-                'fans_group_sensitive_info': {
-                  'type': 'long'
-                },
-                'self_info_sensitive_info': {
+                'total_post_sum': {
                   'type': 'long'
                 },
                 'warning_report_total_sensitive_info': {
                   'type': 'long'
                 },
-                'feedback_total_sensitive_info': {
+                'hot_follower_num': {
+                  'type': 'long'
+                },
+                'trace_follow_tweet_num': {
+                  'type': 'long'
+                },
+                'self_info_sensitive_info': {
+                  'type': 'long'
+                },
+                'retweet_day_num': {
                   'type': 'long'
                 }
               }
