4a08e7c2655f5e7e997214484b83359bf9eccd7d
linhaobuaa <1257819385@qq.com>
2018-03-27 15:22:35 +0800

    ymm

--- xnr/static/js/postFaceBook-1.js
+++ xnr/static/js/postFaceBook-1.js
@@ -484,7 +484,30 @@ $('#sure_post').on('click',function () {
     // if (rank==7){post_url_1+='&rankid='+rankidList.join(',')};
     public_ajax.call_request('get',post_url_1,postYES)
 });
-
+//一键审核
+$("#oneClick").on('click',function () {
+    var txt=Check($('#post-2-content').text());
+    if(!txt){
+        $('#pormpt p').text('请检查您的发帖内容，不能为空。');
+        $('#pormpt').modal('show');
+        return false;
+    }
+    var oneJugement_url='/intelligent_writing/one_click_evaluation/?text='+txt;
+    public_ajax.call_request('get',oneJugement_url,oneJugement);
+});
+function oneJugement(data) {
+    $('#oneJuge .one-1 b').text(data[0]);
+    var word='<span style="display:inline-block;">敏感词：</span>';
+    if(data[1].length==0){
+        word+='<b style="color:salmon;display:inline-block;margin:0 5px;">暂无敏感词。</b>';
+    }else {
+        $.each(data[1],function (index,item) {
+            word+='<b style="color:salmon;display:inline-block;margin:0 5px;">'+item+'</b>';
+        })
+    }
+    $('#oneJuge .one-2').html(word);
+    $('#oneJuge').modal('show');
+}
 //群可见的情况
 // var rankidList=[];
 // function groupSure() {
