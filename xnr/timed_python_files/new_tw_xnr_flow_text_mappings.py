161da80c4adfd2c38f51bb002d4f1bf8331a65be
linhaobuaa <1257819385@qq.com>
2018-04-10 17:08:44 +0800

    qxk

--- xnr/timed_python_files/new_tw_xnr_flow_text_mappings.py
+++ xnr/timed_python_files/new_tw_xnr_flow_text_mappings.py
@@ -6,7 +6,7 @@ from global_utils import es_xnr as es, new_tw_xnr_flow_text_index_name_pre, \
                 new_tw_xnr_flow_text_index_type
 
 
-def tw_xnr_flow_text_mappings(index_name):
+def new_tw_xnr_flow_text_mappings(index_name):
     index_info = {
             'settings':{
                 'analysis':{
@@ -21,114 +21,117 @@ def tw_xnr_flow_text_mappings(index_name):
             'mappings':{
                 'text':{
                     'properties':{
-                        'task_source':{  # 日常发帖，热点跟随，业务发帖
+                        'task_source':{  # 日常发帖，热点跟随，业务发帖，此处空着
                             'type':'string',
                             'index':'not_analyzed'
                         },
-                        'xnr_user_no':{
+                        'xnr_user_no':{ #虚拟人编号
                             'type':'string',
                             'index':'not_analyzed'
                         },
-                        'uid':{
+                        'uid':{ #虚拟人uid
                             'type':'string',
                             'index':'not_analyzed'
                         },
-                        'text':{
+                        'text':{ #文本内容
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'picture_url':{
+                        'picture_url':{ #图片url
                             'type':'string',
                             'index':'not_analyzed'
                         },
-                        'vedio_url':{
+                        'vedio_url':{ #视频url
                             'type':'string',
                             'index':'not_analyzed'
                         },
-                        'user_fansnum':{
+                        'user_fansnum':{ #粉丝数
                             'type':'long'
                         },
-                        'user_followersum':{
+                        'user_followersum':{ #关注者数
                             'type':'long'
                         },
-                        'weibos_sum':{
+                        'weibos_sum':{ #发帖总数
                             'type':'long'
                         },
-                        'tid':{
+                        'tid':{ #帖子id
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'ip':{
+                        'ip':{ #IP地址
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'directed_uid':{
+                        'directed_uid':{#直接转发帖子的源uid
                             'type':'long',
                             },
-                        'directed_uname':{
+                        'directed_uname':{#直接转发帖子的源用户昵称
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'timestamp':{
+                        'timestamp':{#时间戳
                             'type': 'long'
                             },
                         'sentiment': {
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'geo':{
+                        'geo':{#地理位置
                             'type': 'string',
                             'analyzer': 'my_analyzer'
                             },
-                        'keywords_dict':{
+                        'keywords_dict':{#关键词dict
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'keywords_string':{
+                        'keywords_string':{ #关键词string
                             'type': 'string',
                             'analyzer': 'my_analyzer'
                             },
-                        'sensitive_words_dict':{
+                        'sensitive_words_dict':{#敏感词dict
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'sensitive_words_string':{
+                        'sensitive_words_string':{#敏感词string
                             'type': 'string',
                             'analyzer': 'my_analyzer'
                             },
-                        'message_type':{
+                        'message_type':{#数据类型，、原创——1、转发——2、评论——3
                             'type': 'long'
                             },
-                        'root_uid':{
+                        'root_uid':{#源头帖子的uid
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'root_tid':{
+                        'root_tid':{#源头帖子的tid
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
                          # uncut weibo text
-                        'origin_text':{
+                        'origin_text':{#源头帖子的文本
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'origin_keywords_dict':{
+                        'origin_keywords_dict':{#源头帖子的dict
                             'type': 'string',
                             'index': 'not_analyzed'
                             },
-                        'origin_keywords_string':{
+                        'origin_keywords_string':{#源头帖子的string
                             'type': 'string',
                             'analyzer': 'my_analyzer'
                             },
-                        'comment':{
+                        'comment':{#评论数
                             'type':'long'
                             },
-                        'sensitive':{
+                        'sensitive':{#敏感度
                             'type':'long'
                             },
-                        'retweeted':{
+                        'retweeted':{#转发数
                             'type':'long'
-                            }
+                            },
+                        'like':{ #点赞数
+                            'type':'long'
+                        }
                         }
                     }
                 }
@@ -166,4 +169,4 @@ def tw_daily_inerests_flow_text_mappings(index_name):
 
 if __name__ == '__main__':
 
-    tw_xnr_flow_text_mappings(index_name)
+    new_tw_xnr_flow_text_mappings(index_name)
